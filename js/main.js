9a9cd596c6d6faea3708c67a869eb4be8a797638
Adam <96756923+ramblingadam@users.noreply.github.com>
2022-05-25 03:41:57 -0500

    big update

--- js/main.js
+++ js/main.js
@@ -38,24 +38,46 @@ month.innerText = now.getMonth() + 1
 day.innerText = now.getDate()
 year.innerText = now.getFullYear()
 
+const daysInMonth = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31,
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
 
 
+
+console.log(daysInMonth['2'])
 
 buttons.forEach(btn => btn.addEventListener('click', e => {
-    console.log(e)
-    console.log(e.target.id)
     switch(e.target.id) {
-        case 'monthUp': month.innerText = +month.innerText + 1
+        case 'monthUp':
+            +month.innerText < 12 ? month.innerText = +month.innerText + 1 : null
+            if(+day.innerText > daysInMonth[month.innerText]) day.innerText = daysInMonth[month.innerText]
         break 
-        case 'monthdown': month.innerText = +month.innerText - 1
+        case 'monthDown':
+            +month.innerText > 1 ? month.innerText= +month.innerText - 1 : null
+            if(+day.innerText > daysInMonth[month.innerText]) day.innerText = daysInMonth[month.innerText]
         break 
-        case 'dayUp': day.innerText = +day.innerText + 1
+        case 'dayUp': +day.innerText < daysInMonth[month.innerText] ? day.innerText = +day.innerText + 1 : null
         break 
-        case 'dayDown': day.innerText = +day.innerText - 1
+        case 'dayDown': +day.innerText > 1 ? day.innerText = +day.innerText - 1 : null
         break 
-        case 'yearUp': year.innerText = +year.innerText + 1
+        case 'yearUp':
+            if(+year.innerText < now.getFullYear()) year.innerText = +year.innerText + 1
+            checkLeapYear()
         break 
-        case 'yearDown': year.innerText = +year.innerText - 1
+        case 'yearDown': 
+            if(+year.innerText > 1995) year.innerText = +year.innerText - 1
+            checkLeapYear()
         break 
     }
 
@@ -65,27 +87,36 @@ submit.addEventListener('click', function() {getApod()})
 
 getApod()
 
+
+// ! ---- Check If Leap Year and Adjust Valid Dates If So/Not ----
+function checkLeapYear() {
+    let currentYear = +year.innerText
+    if((currentYear % 4 === 0 && currentYear % 100 !== 0) || (currentYear % 4 === 0 && currentYear % 400 === 0)) {
+        daysInMonth['2'] = 29
+    } else {
+        console.log('we in here')
+        daysInMonth['2'] = 28
+        if(+month.innerText === 2 && +day.innerText > 28) day.innerText = 28
+    }
+}
+
+
+
 // ! ---- Get Picture/Movie of the Day Function ----
 function getApod() {
-    // console.log(dateInput.value)
-    // // Check for future/invalid date:
-    console.log(Date.parse(`${year.innerText}-${month.innerText}-${day.innerText}`))
-    console.log(Date.parse(now))
-    if(Date.parse(`${year.innerText}-${month.innerText}-${day.innerText}` > Date.parse(now))) {
-        description.innerText('ERROR: Future Dade Selected')
-    }
-    // // Good date input:
-    else {
-        fetch(`https://api.nasa.gov/planetary/apod?api_key=NkhYWq3XrJ0OKZacsNdXnpIxW1tcWv2i1Wga9eRD&date=${year.innerText}-${month.innerText}-${day.innerText}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            displayMedia(data)
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        })
-    }
+    description.innerText = 'Loading...'
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=NkhYWq3XrJ0OKZacsNdXnpIxW1tcWv2i1Wga9eRD&date=${year.innerText}-${month.innerText}-${day.innerText}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        // If APOD doesn't exist (error code in response), show error msg.
+        if(data.code === 400) {
+            description.innerText = data.msg
+        } else displayMedia(data) // Otherwise, we're good! Display APOD and message.
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
 }
 
 // ! ---- Display Media Function ----
